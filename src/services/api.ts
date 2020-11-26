b2c9d24f9b4bc1fb9e0c9b71b33849bdc40f785b
Lucas Del Puerto <lucasgarciadragon@hotmail.com>
2020-11-26 12:31:32 -0300

    :rocket: Preparing to deploy

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
+const development = process.env.NODE_ENV !== 'production'
+
 export default axios.create({
-    baseURL: 'http://localhost:8888'
+    baseURL: 'https://src-backend-spring-holmes.herokuapp.com/'
 });
